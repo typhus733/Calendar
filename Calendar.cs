8474604c55dda566a5436b9db60fb79a71350aad
Brandon Gunthner <typhus733@gmail.com>
2021-11-05 17:43:19 -0500

    Added contact class and menu, need to update previous methods to handle a contact

--- Calendar.cs
+++ Calendar.cs
@@ -10,6 +10,7 @@ namespace Calendar
         private string location;
         private DateTime startDateTime;
         private DateTime endDateTime;
+        private Contact contact1;
 
         public Meeting(string title, string location, DateTime start, DateTime end)
         {
@@ -17,6 +18,27 @@ namespace Calendar
             Location = location;
             StartDateTime = start;
             EndDateTime = end;
+        }
+
+        public Meeting(string title, string location, DateTime start, DateTime end, Contact contact)
+        {
+            Title = title;
+            Location = location;
+            StartDateTime = start;
+            EndDateTime = end;
+            Contact1 = contact;
+        }
+
+        public Contact Contact1
+        {
+            get
+            {
+                return this.contact1;
+            }
+            set
+            {
+                this.contact1 = value;
+            }
         }
 
         public string Title
@@ -67,8 +89,78 @@ namespace Calendar
             }
         }
     }
-    
-    
+
+
+    public class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string phone;
+        private string email;
+
+        public Contact(string firstName, string lastName, string phone, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Email = email;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return this.phone;
+            }
+            set
+            {
+                this.phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+            set
+            {
+                this.email = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return (FirstName + " " + LastName + " - " + Phone + " - " + Email);
+        }
+    }
+
+
     class Calendar
     {
         static List<Meeting> CalendarFill(string[] calendar) 
@@ -84,7 +176,25 @@ namespace Calendar
             }
 
             return currentCalendar;
-        } 
+        }
+
+        static List<Contact> ContactFill()
+        {
+            List<Contact> contactList = new List<Contact>();
+            string[] contactStrings = File.ReadAllLines(@"C:\Users\brandon.gunthner\Desktop\contacts.dat");
+
+
+            foreach (string line in contactStrings)
+            {
+
+                string[] lineSplit = line.Split(",");
+                Contact contactLine = new Contact(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3]);
+                contactList.Add(contactLine);
+            }
+
+            return contactList;
+        }
+
 
         static void GetEvent(List<Meeting> calendar)
         {
@@ -141,6 +251,91 @@ namespace Calendar
             Console.Write("\n");
         }
 
+        static void CalendarSave(List<Meeting> calendar, string location)
+        {
+            string saveString = "";
+            foreach (Meeting meeting in calendar)
+            {
+                saveString += (meeting.Title + "," + meeting.Location + "," + meeting.StartDateTime.ToString() + "," + meeting.EndDateTime.ToString() + "\n");
+            }
+            File.WriteAllText(location, saveString);
+            Console.WriteLine("\nSaved Successfully\n");
+        }
+
+        static void ContactsSave(List<Contact> contacts)
+        {
+            string saveString = "";
+            foreach (Contact contact in contacts)
+            {
+                saveString += (contact.FirstName + "," + contact.LastName + "," + contact.Phone + "," + contact.Email + "\n");
+            }
+            File.WriteAllText(@"C:\Users\brandon.gunthner\Desktop\contacts.dat", saveString);
+            Console.WriteLine("\nSaved Successfully\n");
+        }
+
+        static void ContactPrint(List<Contact> contacts)
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine("{0} {1} - {2} - {3}", contact.FirstName, contact.LastName, contact.Phone, contact.Email);
+            }
+            Console.WriteLine("-----------------\n");
+        }
+
+        static void ContactAdd(List<Contact> contacts)
+        {
+            Console.Write("Enter Contact First Name: ");
+            string first = Console.ReadLine();
+            Console.Write("Enter Contact Last Name: ");
+            string last = Console.ReadLine();
+            Console.Write("Enter Contact Phone Number: ");
+            string phone = Console.ReadLine();
+            Console.Write("Enter Contact Email: ");
+            string email = Console.ReadLine();
+            foreach (Contact contact in contacts)
+            {
+                if (contact.FirstName.ToLower() == first.ToLower() && contact.LastName.ToLower() == last.ToLower() 
+                    && contact.Phone.ToLower() == phone.ToLower() && contact.Email.ToLower() == email.ToLower())
+                {
+                    Console.WriteLine("Contact already exists, function canceled\n");
+                    return;
+                }
+            }
+
+            Contact newContact = new Contact(first, last, phone, email);
+
+            contacts.Add(newContact);
+
+            Console.WriteLine("\nContact added successfully\n");
+        }
+
+        static void ContactRemove(List<Contact> contacts)
+        {
+            Console.Write("Enter Contact First Name: ");
+            string first = Console.ReadLine();
+            Console.Write("Enter Contact Last Name: ");
+            string last = Console.ReadLine();
+            for (int x = contacts.Count - 1; x > -1; x--)
+            {
+                if (contacts[x].FirstName.ToLower() == first.ToLower() && contacts[x].LastName.ToLower() == last.ToLower())
+                {
+                    Console.WriteLine("Delete Contact: {0} {1} - {2} - {3}", contacts[x].FirstName, contacts[x].LastName, contacts[x].Phone, contacts[x].Email);
+                    Console.Write("Enter Y to confirm, any other key to not delete contact: ");
+                    string confirm = Console.ReadLine();
+
+                    if (confirm == "Y" || confirm == "y")
+                    {
+                        contacts.RemoveAt(x);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+        }
+
         static void CalendarMenu(List<Meeting> calendar)
         {
             bool run = true;
@@ -167,7 +362,7 @@ namespace Calendar
                         int found = 0;
                         for (int x = calendar.Count-1; x > -1; x--)
                         {   
-                            if (calendar[x].Title == title)
+                            if (calendar[x].Title.ToLower() == title.ToLower())
                             {
                                 calendar.RemoveAt(x);
                                 found = 1;
@@ -194,24 +389,66 @@ namespace Calendar
             } while (run == true);
         }
 
-        static void CalendarSave(List<Meeting> calendar, string location)
+        static void ContactMenu(List<Contact> contacts)
         {
-            string saveString = "";
-            foreach (Meeting meeting in calendar)
+            bool run = true;
+            do
             {
-                saveString += (meeting.Title + "," + meeting.Location + "," + meeting.StartDateTime.ToString() + "," + meeting.EndDateTime.ToString() + "\n");
+                try
+                {
+                    Console.WriteLine("Enter a menu option 1-4:\n1 - View Contacts\n2 - Make a new Contact\n3 - Remove a Contact\n4 - Exit menu and save");
+                    string menuInput = Console.ReadLine();
+                    switch (menuInput)
+                    {
+                        case "1":
+                            Console.WriteLine("-----------------");
+                            ContactPrint(contacts);
+                            break;
+                        case "2":
+                            ContactAdd(contacts);
+                            break;
+                        case "3":
+                            ContactRemove(contacts);
+                            break;
+                        case "4":
+                            ContactsSave(contacts);
+                            run = false;
+                            break;
+                        default:
+                            Console.WriteLine("Input not valid");
+                            break;
+
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error updating contacts, check file");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error updating contacts, check file");
+                }
             }
-            File.WriteAllText(location, saveString);
-            Console.WriteLine("\nSaved Successfully\n");
+            while (run == true);
         }
         
         static void Main(string[] args)
         {
             bool run = true;
             List<Meeting> currentCalendar = new List<Meeting>();
+            List<Contact> contactList = new List<Contact>();
+            try
+            {
+                contactList = ContactFill();
+            }
+            catch
+            {
+                Console.WriteLine("Warning! Error reading contacts, check file");
+            }
+
             do
             {
-                Console.WriteLine("Enter a menu option 1-3:\n1 - Load an existing Calendar\n2 - Make a new Calendar\n3 - Exit program");
+                Console.WriteLine("Enter a menu option 1-4:\n1 - Load an existing Calendar\n2 - Make a new Calendar\n3 - Add/View Contacts\n4 - Exit program");
                 string topLevelInput = Console.ReadLine();
                 try
                 {
@@ -237,6 +474,11 @@ namespace Calendar
                             break;
 
                         case "3":
+                            Console.WriteLine("");
+                            ContactMenu(contactList);
+                            break;
+
+                        case "4":
 
                             run = false;
                             break;
